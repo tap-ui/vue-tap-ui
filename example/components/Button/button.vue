136ab605bf9a51b32426c183195fffb614c2fa2f
liwen <915355753@qq.com>
2018-03-09 00:25:28 +0800

    按钮接近完成

--- example/components/Button/button.vue
+++ example/components/Button/button.vue
@@ -4,15 +4,30 @@
       <h1>主题</h1>
       <tap-button>default</tap-button>
       <tap-button type="primary">primary</tap-button>
-      <tap-button type="danger">danger</tap-button>
       <tap-button type="success">success</tap-button>
+      <tap-button type="danger">danger</tap-button>
       <tap-button type="warning">warning</tap-button>
     </section>
     <section>
       <h1>尺寸</h1>
       <tap-button size='small'>small</tap-button>
+      <tap-button>默认</tap-button>
       <tap-button size='large'>large</tap-button>
       <tap-button size='block'>block</tap-button>
+    </section>
+    <section>
+      <h1>
+        幽灵按钮
+      </h1>
+      <tap-button plain>幽灵按钮</tap-button>
+      <tap-button plain type='primary'>primary</tap-button>
+      <tap-button plain type='success'>success</tap-button>
+      <tap-button plain type='danger'>danger</tap-button>
+      <tap-button plain type='warning'>warning</tap-button>
+    </section>
+    <section>
+      <h1>禁用</h1>
+      <tap-button type='primary' disable>primary</tap-button>
     </section>
   </div>
 </template>
--- src/components/button/button.vue
+++ src/components/button/button.vue
@@ -3,9 +3,11 @@
     :type="type"
     class="tap-button"
     :class="['tap-button--'+ type, 'tap-button--'+size, {
-      'is-disable':disable
+      'is-plain':plain,
+      'is-disable':disabled
     }]"
     @click="handleClick"
+    :disabled='disabled'
   >
     <slot></slot>
   </button>
@@ -20,13 +22,17 @@
       }
     },
     props: {
+      plain: Boolean,
+      disabled: Boolean,
       type:{
         default:'default',
         validator(value) {
           return [
             'default',
             'danger',
-            'primary'
+            'primary',
+            'success',
+            'warning'
           ].findIndex((_value, index) => {
             return _value == value
           }) > -1
@@ -40,11 +46,10 @@
             'normal',
             'large',
             'block'
-          ]
+          ].findIndex((_value, index) => {
+            return _value == value
+          }) > -1
         }
-      },
-      disable: {
-        default: false
       }
     }
   }
@@ -61,56 +66,107 @@
       box-sizing: border-box;
       color: inherit;
       display: block;
-      font-size: 18px;
+      font-size: 16px;
       height: 41px;
       outline: 0;
       overflow: hidden;
       position: relative;
       text-align: center;
 
-      /* 主题 */
+      /* 点击的active样式 */
+      &::after {
+        background-color: #fff;
+        content: " ";
+        opacity: 0;
+        position: absolute 0 0 0 0;
+        box-shadow:0 0 10px 1px rgba(0,0,0,0.22) inset;
+      }
+      &:not(.is-disable):active::after {
+        opacity: .4;
+      }
 
+      /* 主题 */
       @modifier default {
         color: $button-default-color;
         background-color: $button-default-background-color;
         box-shadow: $button-default-box-shadow;
+
+        @when plain {
+         border: 1px solid $button-default-plain-color;
+         background-color: transparent;
+         box-shadow: none;
+         color: $button-default-plain-color;
+        }
       }
       @modifier primary {
         color: $button-write-color;
         background-color: $button-primary-background-color;
+
+        @when plain {
+          border:1px solid $button-primary-background-color;
+          background-color: $button-write-color;
+          color: $button-primary-background-color;
+        }
       }
       @modifier danger{
         color: $button-write-color;
         background-color: $button-danger-background-color;
+
+        @when plain {
+          border: 1px solid $button-danger-background-color;
+          background-color: $button-write-color;
+          color: $button-danger-background-color;
+        }
       }
       @modifier success{
         color: $button-write-color;
         background-color: $button-success-backrgound-color;
+
+        @when plain {
+          border: 1px solid $button-success-backrgound-color;
+          background-color: $button-write-color;
+          color: $button-success-backrgound-color;
+        }
       }
       @modifier warning{
         color: $button-write-color;
         background-color: $button-warning-backrgound-color;
+
+        @when plain {
+          border: 1px solid $button-warning-backrgound-color;
+          background-color: $button-write-color;
+          color: $button-warning-backrgound-color;
+        }
       }
 
       /* 尺寸 */
       @modifier small {
-        width: 50px;
-        fong-size: 14px;
+        font-size: 14px;
+        display: inline-block;
+        height: 33px;
+        padding: 0px 10px;
+      }
+      @modifier normal {
+        font-size: 16px;
+        display: inline-block;
+        padding: 2px 10px;
+      }
+      @modifier large {
+        display: inline-block;
+        font-size: 20px;
+        padding: 6px 10px;
+        height: 51px;
       }
       @modifier block {
         width: 100%;
         display: block;
       }
-      @modifier normal {
-        width: 100px;
 
-        padding: 0 12px;
+      /* 禁用 */
+      @when disabled {
+        opacity: .4;
       }
     }
-
-
-
-
   }
 
 </style>
